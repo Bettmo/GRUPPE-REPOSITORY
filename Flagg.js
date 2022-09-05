de57541c1a5ef83a8957d07d77b0f9f69fe944e3
Eric <ericbbettmo@gmail.com>
2022-09-05 17:36:15 +0200

    do it

---
+++ Flagg.js
@@ -0,0 +1,47 @@
+let NOcanvas = document.querySelector('#Norge');
+let c1 = NOcanvas.getContext('2d');
+
+c1.fillStyle = 'red';
+c1.fillRect(0,0,60,60);
+c1.fillStyle = 'blue';
+c1.fillRect(70,0,20,150);
+c1.fillStyle = 'blue';
+c1.fillRect(0,70,200,20);
+c1.fillStyle = 'red';
+c1.fillRect(0,100,60,60);
+c1.fillStyle = 'red';
+c1.fillRect(100,0,100,60);
+c1.fillStyle = 'red';
+c1.fillRect(100,100,100,60);
+
+let DKcanvas = document.querySelector('#DK');
+let c2 = DKcanvas.getContext('2d');
+
+c2.fillStyle = 'blue';
+c2.fillRect(0,0,60,60);
+c2.fillStyle = 'red';
+c2.fillRect(70,0,20,150);
+c2.fillStyle = 'red';
+c2.fillRect(0,70,200,20);
+c2.fillStyle = 'blue';
+c2.fillRect(0,100,60,60);
+c2.fillStyle = 'blue';
+c2.fillRect(100,0,100,60);
+c2.fillStyle = 'blue';
+c2.fillRect(100,100,100,60);
+
+let SEcanvas = document.querySelector('#SE');
+let c3 = SEcanvas.getContext('2d');
+
+c3.fillStyle = '#006AA7';
+c3.fillRect(0,0,60,60);
+c3.fillStyle = '#FECC02';
+c3.fillRect(70,0,20,150);
+c3.fillStyle = '#FECC02';
+c3.fillRect(0,70,200,20);
+c3.fillStyle = '#006AA7';
+c3.fillRect(0,100,60,60);
+c3.fillStyle = '#006AA7';
+c3.fillRect(100,0,100,60);
+c3.fillStyle = '#006AA7';
+c3.fillRect(100,100,100,60);
