c5496b568d67bff969b2521fe7d6b0cbf9be84ff
HermanKE <herman.kjosterud.erlingsen@gmail.com>
2022-09-06 20:36:13 +0200

    dimentions

--- Flagoppave mappe/flagoppgave.js
+++ Flagoppave mappe/flagoppgave.js
@@ -3,48 +3,48 @@ let c1 = NOcanvas.getContext('2d');
 
 c1.fillStyle = '#EF2B2D'; // rød
 c1.fillRect(0,0,60,60);
-c1.fillStyle = '#002868'; // blå
+c1.fillStyle = '#002868'; // blå v
 c1.fillRect(70,0,20,150);
-c1.fillStyle = '#002868'; // blå
-c1.fillRect(0,70,200,20);
+c1.fillStyle = '#002868'; // blå h
+c1.fillRect(0,70,220,20);
 c1.fillStyle = '#EF2B2D'; // rød
 c1.fillRect(0,100,60,60);
 c1.fillStyle = '#EF2B2D'; // rød
-c1.fillRect(100,0,100,60);
+c1.fillRect(100,0,120,60);
 c1.fillStyle = '#EF2B2D'; // rød
-c1.fillRect(100,100,100,60);
+c1.fillRect(100,100,120,60);
 
 let IScanvas = document.querySelector('#IS');
 let c2 = IScanvas.getContext('2d');
 
 c2.fillStyle = '#024f9c'; // blå
 c2.fillRect(0,0,60,60);
-c2.fillStyle = '#dc1e35'; // rød
+c2.fillStyle = '#dc1e35'; // rød v
 c2.fillRect(70,0,20,150);
-c2.fillStyle = '#dc1e35'; // rød
-c2.fillRect(0,70,200,20);
+c2.fillStyle = '#dc1e35'; // rød h
+c2.fillRect(0,70,217,20);
 c2.fillStyle = '#024f9c'; // blå
 c2.fillRect(0,100,60,60);
 c2.fillStyle = '#024f9c'; // blå
-c2.fillRect(100,0,100,60);
+c2.fillRect(100,0,117,60);
 c2.fillStyle = '#024f9c'; // blå
-c2.fillRect(100,100,100,60);
+c2.fillRect(100,100,117,60);
 
 let SEcanvas = document.querySelector('#SE');
 let c3 = SEcanvas.getContext('2d');
 
-c3.fillStyle = '#006AA7';
-c3.fillRect(0,0,60,60);
-c3.fillStyle = '#FECC02';
-c3.fillRect(70,0,20,150);
-c3.fillStyle = '#FECC02';
-c3.fillRect(0,70,200,20);
-c3.fillStyle = '#006AA7';
-c3.fillRect(0,100,60,60);
-c3.fillStyle = '#006AA7';
-c3.fillRect(100,0,100,60);
-c3.fillStyle = '#006AA7';
-c3.fillRect(100,100,100,60);
+c3.fillStyle = '#006AA7'; // blå
+c3.fillRect(0,0,75,60);
+c3.fillStyle = '#FECC02'; // gul v
+c3.fillRect(60,0,30,150);
+c3.fillStyle = '#FECC02'; // gul h
+c3.fillRect(0,60,240,30);
+c3.fillStyle = '#006AA7'; // blå
+c3.fillRect(0,90,60,75);
+c3.fillStyle = '#006AA7'; // blå
+c3.fillRect(90,0,150,60);
+c3.fillStyle = '#006AA7'; // blå
+c3.fillRect(90,90,150, 60);
 
 let CARcanvas = document.querySelector('#CAR');
 let c4 = CARcanvas.getContext('2d');
