d2947794948e07e09be8490bf376469def3213dc
Eric <106038230+Bettmo@users.noreply.github.com>
2022-09-11 23:00:01 +0200

    Island og norge fix

--- flagoppgave.js
+++ flagoppgave.js
@@ -1,6 +1,8 @@
 let NOcanvas = document.querySelector('#Norge');
 let c1 = NOcanvas.getContext('2d');
 
+c1.fillStyle = '#FFFFFf'; // rød
+c1.fillRect(0,0,220,150);
 c1.fillStyle = '#EF2B2D'; // rød
 c1.fillRect(0,0,60,60);
 c1.fillStyle = '#002868'; // blå v
@@ -17,6 +19,8 @@ c1.fillRect(100,100,120,60);
 let IScanvas = document.querySelector('#IS');
 let c2 = IScanvas.getContext('2d');
 
+c2.fillStyle = '#FFFFFf'; // rød
+c2.fillRect(0,0,220,150);
 c2.fillStyle = '#024f9c'; // blå
 c2.fillRect(0,0,60,60);
 c2.fillStyle = '#dc1e35'; // rød v
