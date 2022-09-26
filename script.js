dd5c5b292a9f01cd715bf470524b2ca07277cbe1
Eric <ericbbettmo@gmail.com>
2022-09-26 05:41:43 +0200

    Legge til en javascript for animasjon

---
+++ script.js
@@ -0,0 +1,7 @@
+/* Animasjon */
+var typed = new Typed('.typing', {
+    strings: ["","Eric Bettmo", "Herman Erlingsen", "Trym", "Umit Yildrim", "Brage", "Vinh"],
+    typeSpeed: 150,
+    backSpeed: 60,
+    loop: true
+})
