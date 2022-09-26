055ea76ec248a9dd6c65c026e51c50fc15b00044
Eric <ericbbettmo@gmail.com>
2022-09-26 05:53:50 +0200

    oppdatert navn

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* Animasjon */
 var typed = new Typed('.typing', {
-    strings: ["","Eric Bettmo", "Herman Erlingsen", "Trym", "Umit Yildrim", "Brage", "Vinh", "12A"],
+    strings: ["","Eric Bettmo", "Herman Erlingsen", "Trym Lundby", "Umit Yildrim", "Brage Kjemperud", "Vinh Nguyen", "12A"],
     typeSpeed: 150,
     backSpeed: 100,
     loop: true
